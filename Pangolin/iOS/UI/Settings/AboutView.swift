b7558367f91bf1e2ca145563eb122c6e602a58de
miloschwartz <mschwartz10612@gmail.com>
2026-01-02 12:07:02 -0500

    improve about view

--- Pangolin/iOS/UI/Settings/AboutView.swift
+++ Pangolin/iOS/UI/Settings/AboutView.swift
@@ -9,10 +9,6 @@
 import SwiftUI
 
 struct AboutView: View {
-    private var appName: String {
-        Bundle.main.infoDictionary?["CFBundleName"] as? String ?? "Pangolin"
-    }
-    
     private var appVersion: String {
         Bundle.main.infoDictionary?["CFBundleShortVersionString"] as? String ?? "Unknown"
     }
@@ -28,89 +24,71 @@ struct AboutView: View {
     
     var body: some View {
         NavigationStack {
-            ScrollView {
-                VStack(spacing: 20) {
-                // App icon and name header
-                VStack(spacing: 12) {
-                    Image("PangolinLogo")
-                        .resizable()
-                        .aspectRatio(contentMode: .fit)
-                        .frame(width: 64, height: 64)
+            Form {
+                Section {
+                    HStack {
+                        Text("Version")
+                        Spacer()
+                        Text("\(appVersion) (\(buildNumber))")
+                            .foregroundColor(.secondary)
+                    }
                     
-                    Text(appName)
-                        .font(.title2)
-                        .fontWeight(.medium)
+                    HStack {
+                        Text("Copyright")
+                        Spacer()
+                        Text(copyright)
+                            .foregroundColor(.secondary)
+                    }
+                } header: {
+                    Text("Application")
                 }
-                .padding(.top, 20)
-                .padding(.bottom, 16)
                 
-                Form {
-                    Section {
+                Section {
+                    Link(destination: URL(string: "https://docs.pangolin.net/")!) {
                         HStack {
-                            Text("Version")
+                            Text("Documentation")
                             Spacer()
-                            Text("\(appVersion) (\(buildNumber))")
+                            Image(systemName: "arrow.up.right.square")
                                 .foregroundColor(.secondary)
+                                .font(.caption)
                         }
-                        
-                        HStack {
-                            Text("Copyright")
-                            Spacer()
-                            Text(copyright)
-                                .foregroundColor(.secondary)
-                        }
-                    } header: {
-                        Text("Application")
                     }
                     
-                    Section {
-                        Link(destination: URL(string: "https://docs.pangolin.net/")!) {
-                            HStack {
-                                Text("Documentation")
-                                Spacer()
-                                Image(systemName: "arrow.up.right.square")
-                                    .foregroundColor(.secondary)
-                                    .font(.caption)
-                            }
+                    Link(destination: URL(string: "https://docs.pangolin.net/about/how-pangolin-works")!) {
+                        HStack {
+                            Text("How Pangolin Works")
+                            Spacer()
+                            Image(systemName: "arrow.up.right.square")
+                                .foregroundColor(.secondary)
+                                .font(.caption)
                         }
-                        
-                        Link(destination: URL(string: "https://docs.pangolin.net/about/how-pangolin-works")!) {
-                            HStack {
-                                Text("How Pangolin Works")
-                                Spacer()
-                                Image(systemName: "arrow.up.right.square")
-                                    .foregroundColor(.secondary)
-                                    .font(.caption)
-                            }
-                        }
-                    } header: {
-                        Text("Resources")
                     }
-                    
-                    Section {
-                        Link(destination: URL(string: "https://pangolin.net/terms-of-service.html")!) {
-                            HStack {
-                                Text("Terms of Service")
-                                Spacer()
-                                Image(systemName: "arrow.up.right.square")
-                                    .foregroundColor(.secondary)
-                                    .font(.caption)
-                            }
-                        }
-                        
-                        Link(destination: URL(string: "https://pangolin.net/privacy-policy.html")!) {
-                            HStack {
-                                Text("Privacy Policy")
-                                Spacer()
-                                Image(systemName: "arrow.up.right.square")
-                                    .foregroundColor(.secondary)
-                                    .font(.caption)
-                            }
-                        }
-                    } header: {
-                        Text("Legal")
-                    }
+                } header: {
+                    Text("Documentation")
                 }
+                
+                Section {
+                    Link(destination: URL(string: "https://pangolin.net/terms-of-service.html")!) {
+                        HStack {
+                            Text("Terms of Service")
+                            Spacer()
+                            Image(systemName: "arrow.up.right.square")
+                                .foregroundColor(.secondary)
+                                .font(.caption)
+                        }
+                    }
+                    
+                    Link(destination: URL(string: "https://pangolin.net/privacy-policy.html")!) {
+                        HStack {
+                            Text("Privacy Policy")
+                            Spacer()
+                            Image(systemName: "arrow.up.right.square")
+                                .foregroundColor(.secondary)
+                                .font(.caption)
+                        }
+                    }
+                } header: {
+                    Text("Legal")
                 }
             }
             .navigationTitle("About")
